19db5229d135dddb598dd0c3c9de4917d7eb1cb3
Leosasha19 <Leosasha19@yandex.ru>
2025-08-11 21:26:21 +0300

    delete stirck mode from main tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './app/App.tsx'
-import {Provider} from "react-redux";
-import {store} from "./redux/store/store.ts";
-import {BrowserRouter} from "react-router-dom";
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './app/App.tsx';
+import { Provider } from 'react-redux';
+import { store } from './redux/store/store.ts';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
-    <BrowserRouter>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </BrowserRouter>
-)
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+);
